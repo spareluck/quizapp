89c36dd84c4236e3eb2b664e919a4e42a366b2fc
spareluck <jonjardim@gmail.com>
2015-10-16 10:05:13 -0700

    50% lifeline

--- js/app.js
+++ js/app.js
@@ -1,86 +1,77 @@
 
 $(document).ready(function() {
 
-	//grab buttons that are not correct. Stick them in an array
-	//find function that randomly picks item from array
-	//then add class and disable buttons. 
-
-	// Finish game over function
 	// create game reset function
-	// replace CSS additions in JS with classnames created in CSS
 
-	
 	var questions = [{
         question: "Which Apollo mission landed the first humans on the Moon?",
         choices: ["Apollo 9", "Apollo 11", "Apollo 3", "Apollo 15"],
         answer: 1, 
         letter: "B",
-        qNum : 0, 
         },
         {
         question: "The reactor at the site of the Chernobyl nuclear disaster is now in which country?",
         choices: ["Lithuania", "Russia","Ukraine", "Moldova"],
         answer: 2,
         letter: "C",
-        qNum : 1,
         },
         {
         question: "Which volcano is best known for its eruption in AD 79 that led to the destruction of the Roman cities of Pompeii and Herculaneum?",
         choices: ["Mount Vesuvius","Mount St. Helens", "Mount Etna", "Mount Pinatubo"],
         answer: 0,
         letter: "A",
-        qNum : 2,
         },
         {
         question: "What country has the largest japanese population outside of Japan?",
         choices: ["United States", "China", "Brazil", "Canada"],
         answer: 2,
         letter: "C",
-        qNum : 3,
         },
         {
         question: "Oceans hold what percentage of the Earth's surface water?",
         choices: ["55%","68%", "85%", "97%"],
         answer: 3,
         letter: "D",
-        qNum : 4,
     }]
    
     var numberCorrect = 0;
     var currentQuestion = 0;
     var numberLifeline = 0;
     var counter = 0;
-	
+    var finalCount;
+    var timer;
+    var wrongChoices = [];
+
+    //grab buttons that are not correct. Stick them in an array
+	//find function that randomly picks item from array
+	//then add class and disable buttons. 
+
+
+	function increaseOdds() {
+		wrongChoices = questions[currentQuestion].choices.slice();
+		for(var i=0; i < questions[currentQuestion].choices.length; i++) {
+			if(i == questions[currentQuestion].answer) {
+				correctChoice = wrongChoices.splice(i, 1);
+			}
+		}
+		var rand = wrongChoices[Math.floor(Math.random() * wrongChoices.length)];
+		console.log(rand);
+		$()
+	} 	
 	function timer() {
-		return setInterval(function () {
+		timer = setInterval(function () {
 	    	counter++;
 	    	$(".counter").text(counter);
 		}, 1000);
 	}
-
-	// clearTimeout pass value returned by timer to start clock over
-	 
-	// Start game, go to first question
-	$('.getStarted button').on('click', function(){
-		hideModal("getStarted");
-		timer();
-	});
-
-
-	$(".answerDiv button").on('click', checkUserGuess);
-
-	$(".help .expertLink").on('click', askExpert);
-
 	function showModal(className) {
-		$("." + className).css("display", "inline");
-		$(".overlay-back").css("display", "inline");	
+		$("." + className).addClass("show").removeClass("hide");
+		$(".overlay-back").addClass("show").removeClass("hide");
 	}
-
-	//add css in style.css in class, then add class. Do not add css in JS
 	
 	function hideModal(className) {
-		$("." + className).css("display", "none");
-		$(".overlay-back").css("display", "none");
+		$("." + className).addClass("hide").removeClass("show");
+		$(".overlay-back").addClass("hide").removeClass("show");
 	}
 	function askExpert() {
 		var $expertLink = $(".expertLink");
@@ -94,17 +85,15 @@ $(document).ready(function() {
 		$expertLink[0].disabled = true;
 	    numberLifeline++;
 	}
-
 	function gameOver() {
 	    showModal("gameOver");
-		$(".gameOver").append("<p>Number of correct answers: " + numberCorrect + "</p><p>Number of Life Lines: " + numberLifeline + "</p>");
-		// Add how many seconds player finished quiz in
-		// Stop timer! 
+	    finalCount = counter;
+	    clearTimeout(timer);
+		$(".gameOver").append("<p>Number of correct answers: <span>" + numberCorrect + "</span> <br />Number of Life Lines Used: <span>" + numberLifeline + "</span><br />And it only took you <span>" + finalCount + "</span> seconds!</p>");
+		
 		// Reset game after 5 seconds
 	}
-
 	function nextQuestion() {
-		
 		if (currentQuestion < 5) {
 			$(".question h2").text(questions[currentQuestion].question);
 			$(".qNum").text(currentQuestion + 1);
@@ -135,13 +124,11 @@ $(document).ready(function() {
 	function updateQuestionNumberWrong() {
 		$(".num" + currentQuestion).html("<i class='fa fa-thumbs-o-down'></i>");
 	}
-
 	function updateChoices() {
 		for(var i = 0; i < 4; i++) {
 			$(".choice" + i).text(questions[currentQuestion].choices[i]);
-		}	
+		}
 	}
-
 	function checkUserGuess() {
 		var userAnswer = $(this).find(".text").text();
 		if(userAnswer === questions[currentQuestion].choices[questions[currentQuestion].answer]) {
@@ -154,9 +141,21 @@ $(document).ready(function() {
 		}
 		currentQuestion++
 		nextQuestion();
-		updateChoices();
+		if(currentQuestion < 5) {
+			updateChoices();
+		}
 	}
-	
+
+	$('.getStarted button').on('click', function(){
+		hideModal("getStarted");
+		timer();
+	});
+
+	$(".answerDiv button").on('click', checkUserGuess);
+
+	$(".help .expertLink").on('click', askExpert);
+
+	$(".help .increaseOdds").on('click', increaseOdds);
 	// Create newGame function after game ends to reset game. 
 
 });
